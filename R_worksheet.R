33fc93cb641e692c772e54b68be335ec74f3f2a3
T-Clarkson <u7524109@anu.edu.au>
2023-12-04 23:11:09 +0000

    Further Set-Up and Data Wrangling in R Worksheet
    Loading libraries, tidying data, and creating a new data frame showing the total number of each species across urban and natural trapping sites

--- R_worksheet.R
+++ R_worksheet.R
@@ -1,4 +1,6 @@
 library(pacman)
+library(dplyr)
+library(tidyr)
 p_load(bookdown, tidyverse, ggforce, flextable, latex2exp, png, magick)
 
 #Setting the pathway for finding the data file
@@ -6,6 +8,16 @@ dros_path <- "./data/raw_data_drosophila.csv"
 #Reading in the CSV file using the preset pathway
 dros_data <- read_csv(dros_path, col_select = -c(7, 8))
 
+#Listing each different species character value
+list(unique(dros_data$species))
+
+#Fixing a spelling mistake identified from the previous step
+dros_data$species[dros_data$species == "subs"] <- "sub"
 
 
+total_abundance_df <- dros_data %>%
+  group_by(species, area_type) %>%
+  summarise(count = n()) %>%
+  pivot_wider(names_from = species, values_from = count, values_fill = 0)
 
+print(total_abundance_df)
